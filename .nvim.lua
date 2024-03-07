728912f240c2662d58aa13e08999b2985a9441ec
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-07 11:22:55 -0400

    chore: remove eslint from .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -7,9 +7,6 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.eslint,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.code_actions.eslint,
   },
 })
 
