f033eb94ac5525c3a973796207b5ec243a3a7489
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-17 12:41:30 -0400

    chore: update .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -3,6 +3,10 @@ local lspconfig = require("lspconfig")
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local function augroup(name)
+  return vim.api.nvim_create_augroup("autocmd_" .. name, { clear = true })
+end
+
 
 null_ls.setup({
   sources = {
@@ -13,4 +17,13 @@ null_ls.setup({
 lspconfig.tsserver.setup({
   capabilities = capabilities,
   on_attach = on_attach,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup(".nvim.lua"),
+  callback = function()
+    if vim.bo.filetype == "javascript" then
+      vim.bo.filetype = "javascriptreact"
+    end
+  end,
 })
