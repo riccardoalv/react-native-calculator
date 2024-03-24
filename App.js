0582c1278004f9dc049fd3cb14eb8a94061135d6
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-24 12:56:40 -0400

    feat: add Work-Sans font

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { CalcButton } from "./components";
+import { keyboardLayout, shape } from "./constants/keyboardLayout";
+import { colors, sizes } from "./constants";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Work-Sans": require("./assets/fonts/Work_Sans/WorkSans-VariableFont_wght.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light-content" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    color: colors.white,
+    backgroundColor: colors.background,
     alignItems: "center",
     justifyContent: "center",
+  },
+  keyboard: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginBottom: sizes.keyboard.height,
   },
 });
